eaea40c454564cb7ace49481156a4fecd622749e
Alex Dyer <>
2020-12-17 09:47:59 +0000

    A very simple game in which the main goal is to simply chop as much wood as you can. I used this to test out a character levelling idea for future projects and i think it was a success! Each time your bar fills up you earn an extra wood per click but it gets progressively harder to level up!

---
+++ src/thegamehw/TheGameHW.java
@@ -0,0 +1,15 @@
+
+package thegamehw;
+
+public class TheGameHW {
+
+  
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+        
+    
+    
+    }
+    
+}
